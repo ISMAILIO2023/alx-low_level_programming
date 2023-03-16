307c0b23cd20c30fb158421aca212416623c0b3a
ismail <italeb.ismail@gmail.com>
2023-03-16 16:04:51 -0700

    printf a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program prints Programming is like building a multilingual puzzle
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
