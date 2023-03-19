5ccf59b7df7699033cba4a62dd3a2c4fad957312
ismail <italeb.ismail@gmail.com>
2023-03-19 12:58:41 -0700

    purchar() lowcase characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Decription: Prints the low case characters using putchar
+ * Return: 0
+ **/
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
