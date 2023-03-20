470b3ebf62f775fa97e32fc83927c13049477568
ismail <italeb.ismail@gmail.com>
2023-03-20 12:38:37 -0700

    Prints integer via putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Decription: Prints the lower using putchar
+ * Return: 0
+ **/
+
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+putchar(i + 48);
+putchar('\n');
+return (0);
+}
