bfcc0090709b537ab9ef88f8a1cd816aecc18f18
ismail <italeb.ismail@gmail.com>
2023-04-21 18:37:26 -0700

    program that prints the opcodes of its own main function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,18 +3,17 @@
 /**
  * print_most_numbers - prints the number from 0 to 9 followed by new line
  * @i : input
- * Return: number
  **/
 
 void print_most_numbers(void)
 {
-	int i;
-	if (i != 2 && i != 4)
-	{
-	for (i = 0; i < 10; i++)
-	  _putchar ( i + '0');
-	_putchar('\n');
-	}
-	return (0);
-}
+		char i;
 
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
